597173966b68676579a48c94df67383fa419247d
Dmitrii Portnov <dimastad@MacBook-Air-Dmitrii.local>
2025-07-21 21:16:49 +0300

    Go Lessons init

---
+++ lesson2/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var name string = "Dimas"
+var age int = 35
+var weight float64 = 76.9
+
+func main() {
+    var isMarried bool = true
+    fmt.Println("Hello Go!")
+    fmt.Println("Имя: ", name)
+    fmt.Println("Возраст: ", age)
+    fmt.Println("Вес: ", weight)
+    fmt.Println("Женат: ", isMarried)
+
+}
