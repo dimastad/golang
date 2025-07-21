592dae6ef35d2b205a64dcd23a8a598602194c09
Dmitrii Portnov <dimastad@MacBook-Air-Dmitrii.local>
2025-07-21 22:36:57 +0300

    lesson5

---
+++ lesson5/loops.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	sum := 0
+
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
+
+	fmt.Println(sum)
+}
